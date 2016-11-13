d5517b0fee2438e6e79e3041d1d4db0f1e222aab
Wenze Gu <nein1994@hotmail.com>
2016-11-12 22:20:37 -0500

    Add files via upload

---
+++ FMP.py
@@ -0,0 +1,195 @@
+import numpy as np
+import re
+import scipy.io as sio
+class Coord:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+	def ReturnP(self, num):
+		if num == 0:
+			return self.first
+		if num == 1:
+			return self.second
+		if num == 2:
+			return self.third
+class Point:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+	def RDis(self, RDisSqu):
+		self.RDisSqu = RDisSqu
+	def BDis(self, BDisSqu):
+		self.BDisSqu = BDisSqu
+class Normal:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+class NormalIndex:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+ff = open("fp.vrml")
+CoordArr = []
+while 1:
+	line = ff.readline()
+	if not line:
+		break
+	m = re.search('coordIndex', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = ff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			coords = [int(x) for x in line.split()]
+			CoordArr.append(Coord(coords[0],coords[1],coords[2], count))
+			count = count + 1
+		break
+ff.close()
+fff = open("fp.vrml")
+PointArr = []
+while 1:
+	line = fff.readline()
+	if not line:
+		break
+	m = re.search('point', line)
+	n = re.search('View', line)
+	o = re.search('grid', line)
+	if bool(m and not n and not o):
+		count = 0
+		while 1:
+			line = fff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			points = [float(x) for x in line.split()]
+			PointArr.append(Point(points[0], points[1], points[2], count))
+			count = count + 1
+		break
+fff.close()
+ffff = open("fp.vrml")
+NormalArr = []
+while 1:
+	line = ffff.readline()
+	if not line:
+		break
+	m = re.search('vector', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = ffff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			normals = [float(x) for x in line.split()]
+			NormalArr.append(Normal(normals[0], normals[1], normals[2], count))
+			count = count + 1
+		break
+ffff.close()
+fffff = open("fp.vrml")
+NormalIndexArr = []
+while 1:
+	line = fffff.readline()
+	if not line:
+		break
+	m = re.search('normalIndex', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = fffff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			normalindices = [int(x) for x in line.split()]
+			NormalIndexArr.append(NormalIndex(normalindices[0], normalindices[1], normalindices[2], count))
+			count = count + 1
+		break
+fffff.close()
+#Start rotation!
+M1 = [[1, 0, 0], [0, -1, 0], [0, 0, -1]]
+M2 = [[-1, 0, 0], [0, 1, 0], [0, 0, -1]]
+M3 = [[-1, 0, 0], [0, -1, 0], [0, 0, 1]]
+PointTable = [[0 for i in range(len(PointArr))] for j in range(3)]
+for Points in PointArr:
+	PointTable[0][Points.num] = Points.first
+	PointTable[1][Points.num] = Points.second
+	PointTable[2][Points.num] = Points.third
+NormalTable = [[0 for i in range(len(NormalArr))] for j in range(3)]
+for Normals in NormalArr:
+	NormalTable[0][Normals.num] = Normals.first
+	NormalTable[1][Normals.num] = Normals.second
+	NormalTable[2][Normals.num] = Normals.third
+data2 = sio.loadmat('fm1.mat')
+data3 = sio.loadmat('fp1.mat')
+#RT = data2['RPT']
+RT1 = np.transpose(data2['RPT'])
+RedTable1=[[0,0],[0,0],[0,0]]
+RedTable2=[[0,0],[0,0],[0,0]]
+#BT = data2['BPT']
+RT2 = np.transpose(data3['RPT'])
+T = [0, 0, 0]
+OutFile = open('foutput', 'w')
+for i in range(12):
+	for j in range(7):
+		matfn = 'f'+str(i+1)+str(j+1)+'.mat'
+		data = sio.loadmat(matfn)
+		Rint = data['Results']
+		for m in range(12):
+			for k in range(7):
+				for n in range(3):
+					RedTable1[n][0]=RT1[n][i]
+					RedTable2[n][0]=RT2[n][j]
+					RedTable1[n][1]=RT1[n][m]
+					RedTable2[n][1]=RT2[n][k]
+				NewRT1 = np.dot(Rint, RedTable1)
+				NewRT2 = np.dot(M1, NewRT1)
+				NewRT3 = np.dot(M2, NewRT1)
+				NewRT4 = np.dot(M3, NewRT1)
+				NewBT1 = np.dot(Rint, RedTable2)
+				NewBT2 = np.dot(M1, NewBT1)
+				NewBT3 = np.dot(M2, NewBT1)
+				NewBT4 = np.dot(M3, NewBT1)
+#print NewRT1
+#print NewBT2
+#print NewBT1
+#print NewRT4
+				a=2*(NewRT1[0][0]-NewBT2[0][0])
+				T[2]=0.5*(NewBT2[2][0]-NewRT1[2][0])
+				#T[1]=0.5*(NewBT4[1][1]-NewRT1[1][1])
+				#T[0]=0.5*(0.5*a+NewBT4[0][1]-NewRT1[0][1])
+				#b=2*(2*T[1]+NewRT1[1][0]-NewBT2[1][0])
+				b=2*(NewRT4[1][1]-NewBT2[1][1])
+				T[0]=0.5*(NewRT4[0][1]-NewBT2[0][1])
+				T[1]=0.5*(0.5*b+NewBT2[1][0]-NewRT1[1][0])
+				c=2*(-2*T[2]+NewBT2[2][1]-NewRT4[2][1])
+				#c=2*(NewRT1[2][1]-NewBT4[2][1])
+				if max(abs(a),abs(b),abs(c))<15.5 and max(abs(a),abs(b),abs(c))>12.5 and min(abs(a),abs(b),abs(c))>3.4 and min(abs(a),abs(b),abs(c))<6.4 and abs(a)+abs(b)+abs(c)-max(abs(a),abs(b),abs(c))-min(abs(a),abs(b),abs(c))>12.1 and abs(a)+abs(b)+abs(c)-max(abs(a),abs(b),abs(c))-min(abs(a),abs(b),abs(c))<15.1:
+					OutFile.write(matfn)
+					OutFile.write('\nT=['+str(T[0])+','+str(T[1])+','+str(T[2])+']\n')
+					OutFile.write('T2=['+str(0.5*a)+','+str(0.5*b)+','+str(0)+']\n')
+					OutFile.write('T3=['+str(0)+','+str(0.5*b)+','+str(0.5*c)+']\n')
+					OutFile.write('T4=['+str(0.5*a)+',0,'+str(0.5*c)+']\n')
+				#T[0]=0.5*(NewBT3[0][1]-NewRT1[0][1])
+				#b=2*(NewRT1[1][1]-NewBT3[1][1])
+				#T[1]=0.5*(0.5*b+NewBT2[1][0]-NewRT1[1][0])
+				#c=2*(2*T[2]+NewRT1[2][1]-NewBT3[2][1])
+				T[0]=0.5*(NewRT3[0][1]-NewBT2[0][1]-0.5*a)
+				T[1]=0.5*(NewBT2[1][1]-NewRT3[1][1])
+				b=2*(2*T[1]+NewRT1[1][0]-NewBT2[1][0])
+				c=2*(NewBT2[2][1]-NewRT3[2][1])
+				if max(abs(a),abs(b),abs(c))<15.5 and max(abs(a),abs(b),abs(c))>12.5 and min(abs(a),abs(b),abs(c))>3.4 and min(abs(a),abs(b),abs(c))<6.4 and abs(a)+abs(b)+abs(c)-max(abs(a),abs(b),abs(c))-min(abs(a),abs(b),abs(c))>12.1 and abs(a)+abs(b)+abs(c)-max(abs(a),abs(b),abs(c))-min(abs(a),abs(b),abs(c))<15.1:
+					OutFile.write(matfn)
+					OutFile.write('\nT=['+str(T[0])+','+str(T[1])+','+str(T[2])+']\n')
+					OutFile.write('T2=['+str(0.5*a)+','+str(0.5*b)+','+str(0)+']\n')
+					OutFile.write('T3=['+str(0)+','+str(0.5*b)+','+str(0.5*c)+']\n')
+					OutFile.write('T4=['+str(0.5*a)+',0,'+str(0.5*c)+']\n')
+OutFile.close()
---
+++ fm1.py
@@ -0,0 +1,381 @@
+import re
+import scipy.io
+class Color:
+	def __init__(self, colorr, colorg, colorb,colornum):
+		self.r = colorr
+		self.g = colorg
+		self.b = colorb
+		self.num = colornum
+	def RDistanceSquare(self):
+		self.RDS = (self.r - 1)*(self.r - 1) + self.g*self.g + self.b*self.b
+		return self.RDS
+	def BDistanceSquare(self):
+		self.BDS = (self.b - 1)*(self.b - 1) + self.g*self.g + self.r*self.r
+		return self.BDS
+class Connection:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+#return the number of the reddist point of this triangle
+	def SetR(self, RNum):
+		self.RMax = RNum
+	def SetB(self, BNum):
+		self.BMax = BNum
+class Coord:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+	def ReturnP(self, num):
+		if num == 0:
+			return self.first
+		if num == 1:
+			return self.second
+		if num == 2:
+			return self.third
+class Point:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+	def RDis(self, RDisSqu):
+		self.RDisSqu = RDisSqu
+	def BDis(self, BDisSqu):
+		self.BDisSqu = BDisSqu
+class Normal:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+class NormalIndex:
+	def __init__(self, first, second, third, num):
+		self.first = first
+		self.second = second
+		self.third = third
+		self.num = num
+fil = open("fm.vrml")
+sStr2 = '] }'
+ColorArr = []
+while 1:
+	line = fil.readline()
+	if not line:
+		break
+	m = re.search('color Color', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = fil.readline()
+			m = re.search(sStr2, line)
+			if bool(m):
+				break
+			floats = [float(x) for x in line.split()]
+			ColorArr.append(Color(floats[0],floats[1],floats[2], count))
+			count = count + 1
+		break
+fil.close()
+f = open("fm.vrml")
+sStr3 = 'colorIndex ['
+sStr4 = ']'
+ConnectionArr = []
+while 1:
+	line = f.readline()
+	if not line:
+		break
+	m = re.search('colorIndex', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = f.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			colorindices = [int(x) for x in line.split()]
+			ConnectionArr.append(Connection(colorindices[0],colorindices[1],colorindices[2], count))
+			count = count + 1
+		break
+for triangle in ConnectionArr:
+	if ColorArr[triangle.first].RDistanceSquare()>ColorArr[triangle.second].RDistanceSquare() and ColorArr[triangle.first].RDistanceSquare()>ColorArr[triangle.third].RDistanceSquare():
+		triangle.SetR(0)
+	if ColorArr[triangle.second].RDistanceSquare()>ColorArr[triangle.first].RDistanceSquare() and ColorArr[triangle.second].RDistanceSquare()>ColorArr[triangle.third].RDistanceSquare():
+		triangle.SetR(1)
+	if ColorArr[triangle.third].RDistanceSquare()>ColorArr[triangle.second].RDistanceSquare() and ColorArr[triangle.third].RDistanceSquare()>ColorArr[triangle.first].RDistanceSquare():
+		triangle.SetR(2)
+	if ColorArr[triangle.first].RDistanceSquare()==ColorArr[triangle.second].RDistanceSquare() and ColorArr[triangle.first].RDistanceSquare()>ColorArr[triangle.third].RDistanceSquare():
+		triangle.SetR(3)
+	if ColorArr[triangle.first].RDistanceSquare()==ColorArr[triangle.third].RDistanceSquare() and ColorArr[triangle.first].RDistanceSquare()>ColorArr[triangle.second].RDistanceSquare():
+		triangle.SetR(4)
+	if ColorArr[triangle.third].RDistanceSquare()==ColorArr[triangle.second].RDistanceSquare() and ColorArr[triangle.first].RDistanceSquare()<ColorArr[triangle.third].RDistanceSquare():
+		triangle.SetR(5)
+	if ColorArr[triangle.first].RDistanceSquare()==ColorArr[triangle.second].RDistanceSquare() and ColorArr[triangle.first].RDistanceSquare()==ColorArr[triangle.third].RDistanceSquare():
+		triangle.SetR(6)
+	if ColorArr[triangle.first].BDistanceSquare()>ColorArr[triangle.second].BDistanceSquare() and ColorArr[triangle.first].BDistanceSquare()>ColorArr[triangle.third].BDistanceSquare():
+		triangle.SetB(0)
+	if ColorArr[triangle.second].BDistanceSquare()>ColorArr[triangle.first].BDistanceSquare() and ColorArr[triangle.second].BDistanceSquare()>ColorArr[triangle.third].BDistanceSquare():
+		triangle.SetB(1)
+	if ColorArr[triangle.third].BDistanceSquare()>ColorArr[triangle.second].BDistanceSquare() and ColorArr[triangle.third].BDistanceSquare()>ColorArr[triangle.first].BDistanceSquare():
+		triangle.SetB(2)
+	if ColorArr[triangle.first].BDistanceSquare()==ColorArr[triangle.second].BDistanceSquare() and ColorArr[triangle.first].BDistanceSquare()>ColorArr[triangle.third].BDistanceSquare():
+		triangle.SetB(3)
+	if ColorArr[triangle.first].BDistanceSquare()==ColorArr[triangle.third].BDistanceSquare() and ColorArr[triangle.first].BDistanceSquare()>ColorArr[triangle.second].BDistanceSquare():
+		triangle.SetB(4)
+	if ColorArr[triangle.third].BDistanceSquare()==ColorArr[triangle.second].BDistanceSquare() and ColorArr[triangle.first].BDistanceSquare()<ColorArr[triangle.third].BDistanceSquare():
+		triangle.SetB(5)
+	if ColorArr[triangle.first].BDistanceSquare()==ColorArr[triangle.second].BDistanceSquare() and ColorArr[triangle.first].BDistanceSquare()==ColorArr[triangle.third].BDistanceSquare():
+		triangle.SetB(6)
+f.close()
+ff = open("fm.vrml")
+CoordArr = []
+while 1:
+	line = ff.readline()
+	if not line:
+		break
+	m = re.search('coordIndex', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = ff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			coords = [int(x) for x in line.split()]
+			CoordArr.append(Coord(coords[0],coords[1],coords[2], count))
+			count = count + 1
+		break
+ff.close()
+fff = open("fm.vrml")
+PointArr = []
+while 1:
+	line = fff.readline()
+	if not line:
+		break
+	m = re.search('point', line)
+	n = re.search('View', line)
+	o = re.search('grid', line)
+	if bool(m and not n and not o):
+		count = 0
+		while 1:
+			line = fff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			points = [float(x) for x in line.split()]
+			PointArr.append(Point(points[0], points[1], points[2], count))
+			count = count + 1
+		break
+fff.close()
+ffff = open("fm.vrml")
+NormalArr = []
+while 1:
+	line = ffff.readline()
+	if not line:
+		break
+	m = re.search('vector', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = ffff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			normals = [float(x) for x in line.split()]
+			NormalArr.append(Normal(normals[0], normals[1], normals[2], count))
+			count = count + 1
+		break
+ffff.close()
+fffff = open("fm.vrml")
+NormalIndexArr = []
+while 1:
+	line = fffff.readline()
+	if not line:
+		break
+	m = re.search('normalIndex', line)
+	if bool(m):
+		count = 0
+		while 1:
+			line = fffff.readline()
+			m = re.search(']', line)
+			if bool(m):
+				break
+			normalindices = [int(x) for x in line.split()]
+			NormalIndexArr.append(NormalIndex(normalindices[0], normalindices[1], normalindices[2], count))
+			count = count + 1
+		break
+fffff.close()
+for APoint in PointArr:
+	for ACoord in CoordArr:
+		if APoint.num == ACoord.first:
+			APoint.RDis(ColorArr[ConnectionArr[ACoord.num].first].RDistanceSquare())
+			break
+		if APoint.num == ACoord.second:
+			APoint.RDis(ColorArr[ConnectionArr[ACoord.num].second].RDistanceSquare())
+			break
+		if APoint.num == ACoord.third:
+			APoint.RDis(ColorArr[ConnectionArr[ACoord.num].third].RDistanceSquare())
+			break
+for APoint in PointArr:
+	for ACoord in CoordArr:
+		if APoint.num == ACoord.first:
+			APoint.BDis(ColorArr[ConnectionArr[ACoord.num].first].BDistanceSquare())
+			break
+		if APoint.num == ACoord.second:
+			APoint.BDis(ColorArr[ConnectionArr[ACoord.num].second].BDistanceSquare())
+			break
+		if APoint.num == ACoord.third:
+			APoint.BDis(ColorArr[ConnectionArr[ACoord.num].third].BDistanceSquare())
+			break
+RR = []
+BB = []
+TempRDisSqu = 1.6
+TempBDisSqu = 0.2
+StrRNum=-1
+StrBNum=-1
+for RPoint in PointArr:
+	if RPoint.RDisSqu < 1.6:
+		RR.append(RPoint)
+		if RPoint.RDisSqu<TempRDisSqu:
+			TempRDisSqu=RPoint.RDisSqu
+			StrRNum=RPoint.num
+#N=0
+#for P in RR:
+#	print('draw p'+str(N)+' {'+str(P.first)+','+str(P.second)+','+str(P.third)+'}')
+#	N = N + 1
+for BPoint in PointArr:
+	if BPoint.BDisSqu < 0.2:
+		BB.append(BPoint)
+		if BPoint.BDisSqu<TempBDisSqu:
+			TempBDisSqu=RPoint.BDisSqu
+			StrBNum=BPoint.num
+import sys
+import numpy as np
+from sklearn.cluster import KMeans
+RMatrix = np.zeros((len(RR),3))
+BMatrix = np.zeros((len(BB),3))
+Temp = 0
+for EachPoint in RR:
+	RMatrix[Temp, 0] = EachPoint.first
+	RMatrix[Temp, 1] = EachPoint.second
+	RMatrix[Temp, 2] = EachPoint.third
+	Temp = Temp + 1
+Temp = 0
+for EachPoint in BB:
+	BMatrix[Temp, 0] = EachPoint.first
+	BMatrix[Temp, 1] = EachPoint.second
+	BMatrix[Temp, 2] = EachPoint.third
+	Temp = Temp + 1
+#clustering analysis
+import itertools
+clf1 = KMeans(n_clusters = 14)
+s1 = clf1.fit(RMatrix)
+clf2 = KMeans(n_clusters = 6)
+s2 = clf2.fit(BMatrix)
+#find the nearest point of the cluster center
+#Nearest1 = 0
+RVMatrix = np.zeros((14, 3))
+BVMatrix = np.zeros((6, 3))
+RNMatrix = np.zeros((14, 3))
+BNMatrix = np.zeros((6, 3))
+count = 0
+hang = 0
+for row in clf1.cluster_centers_:
+	D = 1
+	for P in PointArr:
+		if (P.first-row[0])**2+(P.second-row[1])**2+(P.third-row[2])**2<D:
+			D = (P.first-row[0])**2+(P.second-row[1])**2+(P.third-row[2])**2
+			Nearest = P.num
+	RNMatrix[hang][0]=PointArr[Nearest].first
+	RNMatrix[hang][1]=PointArr[Nearest].second
+	RNMatrix[hang][2]=PointArr[Nearest].third
+	hang = hang + 1
+hang = 0
+for row in clf2.cluster_centers_:
+	D = 1
+	for P in PointArr:
+		if (P.first-row[0])**2+(P.second-row[1])**2+(P.third-row[2])**2<D:
+			D = (P.first-row[0])**2+(P.second-row[1])**2+(P.third-row[2])**2
+			Nearest = P.num
+	BNMatrix[hang][0]=PointArr[Nearest].first
+	BNMatrix[hang][1]=PointArr[Nearest].second
+	BNMatrix[hang][2]=PointArr[Nearest].third
+	hang = hang + 1
+#end of nearest
+count = 0
+#StrRTemp=10000
+#StrBTemp=10000
+#StrR=-1
+#StrB=-1
+#for row in clf1.cluster_centers_:
+for row in RNMatrix:
+	#Distance1 = 1
+	cls1 = []
+	xsum=0
+	ysum=0
+	zsum=0
+	for APoint in PointArr:
+		Distance = (row[0]-APoint.first)**2+(row[1]-APoint.second)**2+(row[2]-APoint.third)**2
+		if Distance < 1:
+			cls1.append(APoint.num)
+			#Distance1 = Distance
+			for Triangles in CoordArr:
+				if APoint.num == Triangles.first:
+					xsum = NormalArr[NormalIndexArr[Triangles.num].first].first+xsum
+					ysum = NormalArr[NormalIndexArr[Triangles.num].first].second+ysum
+					zsum = NormalArr[NormalIndexArr[Triangles.num].first].third+zsum
+					break
+				if APoint.num == Triangles.second:
+					xsum = NormalArr[NormalIndexArr[Triangles.num].second].first+xsum
+					ysum = NormalArr[NormalIndexArr[Triangles.num].second].second+ysum
+					zsum = NormalArr[NormalIndexArr[Triangles.num].second].third+zsum
+					break
+				if APoint.num == Triangles.third:
+					xsum = NormalArr[NormalIndexArr[Triangles.num].third].first+xsum
+					ysum = NormalArr[NormalIndexArr[Triangles.num].third].second+ysum
+					zsum = NormalArr[NormalIndexArr[Triangles.num].third].third+zsum
+					break
+	RVMatrix[count,0]=xsum/len(cls1)
+	RVMatrix[count,1]=ysum/len(cls1)
+	RVMatrix[count,2]=zsum/len(cls1)
+		#StrR=count
+	count = count + 1
+#Nearest2 = 0
+count = 0
+#for row in clf2.cluster_centers_:
+for row in BNMatrix:
+	#Distance2 = 1
+	cls2=[]
+	xsum=0
+	ysum=0
+	zsum=0
+	for APoint in PointArr:
+		Distance = (row[0]-APoint.first)**2+(row[1]-APoint.second)**2+(row[2]-APoint.third)**2
+		if Distance < 1:
+			cls2.append(APoint.num)
+			#Distance2 = Distance
+			for Triangles in CoordArr:
+				if APoint.num == Triangles.first:
+					xsum = NormalArr[NormalIndexArr[Triangles.num].first].first+xsum
+					ysum = NormalArr[NormalIndexArr[Triangles.num].first].second+ysum
+					zsum = NormalArr[NormalIndexArr[Triangles.num].first].third+zsum
+					break
+				if APoint.num == Triangles.second:
+					xsum = NormalArr[NormalIndexArr[Triangles.num].second].first+xsum
+					ysum = NormalArr[NormalIndexArr[Triangles.num].second].second+ysum
+					zsum = NormalArr[NormalIndexArr[Triangles.num].second].third+zsum
+					break
+				if APoint.num == Triangles.third:
+					xsum = NormalArr[NormalIndexArr[Triangles.num].third].first+xsum
+					ysum = NormalArr[NormalIndexArr[Triangles.num].third].second+ysum
+					zsum= NormalArr[NormalIndexArr[Triangles.num].third].third+zsum
+					break
+	BVMatrix[count,0]=xsum/len(cls2)
+	BVMatrix[count,1]=ysum/len(cls2)
+	BVMatrix[count,2]=zsum/len(cls2)
+	count = count + 1
+for i in range(14):
+	print('draw ptr'+str(i)+' {'+str(RNMatrix[i][0])+','+str(RNMatrix[i][1])+','+str(RNMatrix[i][2])+'}')
+scipy.io.savemat('fm1.mat', {'RPT':RNMatrix, 'BPT':BNMatrix, 'RVT':RVMatrix, 'BVT':BVMatrix})
+#scipy.io.savemat('zzzstr.mat',{'RPT':clf1.cluster_centers_[StrR,:],'BPT':clf2.cluster_centers_[StrB,:],'RVT':RVMatrix[StrR,:],'BVT':BVMatrix[StrB,:],'RT':clf1.cluster_centers_,'BT':clf2.cluster_centers_})
